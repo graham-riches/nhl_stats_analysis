804424273392abe6d071c3903fc3da25f7f91b14
Graham Riches <graham@attabotics.com>
2021-01-12 08:19:36 -0700

    Added seasonal experience based adjustments to the projection model.

--- analysis/projections.py
+++ analysis/projections.py
@@ -18,3 +18,23 @@ def weighted_average(games_played: int, weights: list, stats: list) -> float:
     """
     return np.divide(np.sum([x[0] * x[1] for x in zip(weights, stats)]), np.sum(weights[0:len(stats)])) * games_played
 
+
+def weighted_average_with_experience_adjustment(games_played: int, average_weights: list,
+                                                seasonal_progression_weights: list, stats: list) -> float:
+    """
+    Performs a weighted average stat projection based on historical data, but also includes productivity multipliers
+    based on the number of seasons played. For example, players that have already played 5+ seasons are unlikely to
+    see a large productivity bump, while players entering their second or third NHL season are likely to see a
+    substantial experience based projection that is heavily downplayed by a simple historical weighted average model.
+    :param games_played: total number of games to project
+    :param average_weights: weighted averaging coefficients
+    :param seasonal_progression_weights: list of seasonal experience weights to add to projections. Considered as a list
+           of number of seasons played. I.e. [1.1, 1.2, 1.3, 1.0, 1.0, 1.0] would provide a 10% projection boost to
+           players that are entering their sophomore season, a 20% boost to players entering their third season, and a
+           30% boost to players entering their fourth season.
+    :param stats: list of the stat category over seasons
+    :return:
+    """
+    progression_multiplier = seasonal_progression_weights[len(stats) - 1]
+    base_projection = np.divide(np.sum([x[0] * x[1] for x in zip(average_weights, stats)]), np.sum(average_weights[0:len(stats)]))
+    return base_projection * games_played * progression_multiplier
